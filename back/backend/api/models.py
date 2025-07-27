5a55c42a08b5483780c7d63b0db6a5ec517d24ab
Alonso Rodriguez <alonsorodriguezcaceres@gmail.com>
2025-07-26 22:36:34 -0400

    Primer commit

---
+++ back/backend/api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Entrada(models.Model):
+    class Entrada(models.Model):
+        texto = models.TextField()
+        fecha = models.DateTimeField(auto_now_add=True)
